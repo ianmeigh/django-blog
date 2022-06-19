0235eef9ea8e88e114efe13711ce308a561d5e5b
ianmeigh <ianmeigh@googlemail.com>
2022-06-19 23:07:10 +0100

    feat: register models in admin panel
    Register Post and Comment models in Admin interface and specify options
    to customize the interface.

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Post, Comment
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "slug", "status", "created_on")
+    search_fields = ["title", "content"]
+    prepopulated_fields = {"slug": ("title",)}
+    list_filter = ("status", "created_on")
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("name", "body", "post", "created_on", "approved")
+    list_filter = ("approved", "created_on")
+    search_fields = ["name", "email", "body"]
