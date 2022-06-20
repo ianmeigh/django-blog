34a5281aafc655c5c26e8c7bd78bb5e7db76bb1a
ianmeigh <ianmeigh@googlemail.com>
2022-06-20 16:16:01 +0100

    feat: configure url for post detail view

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path("", views.PostListView.as_view(), name="home"),
+    path("<slug:slug>/", views.PostDetailView.as_view(), name="post_detail"),
 ]
