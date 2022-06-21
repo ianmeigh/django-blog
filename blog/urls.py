d7ec28e476c3d03e5bc93914df03216217eb4749
ianmeigh <ianmeigh@googlemail.com>
2022-06-21 21:20:07 +0100

    feat: add url to the post like view

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 urlpatterns = [
     path("", views.PostListView.as_view(), name="home"),
     path("<slug:slug>/", views.PostDetailView.as_view(), name="post_detail"),
+    path("like/<slug:slug>/", views.PostLikeView.as_view(), name="post_like"),
 ]
